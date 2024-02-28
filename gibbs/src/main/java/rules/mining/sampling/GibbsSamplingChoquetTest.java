1a7a8004ce660a165961452b96078cceab6844e7
Tudor1415 <tudoropran14@gmail.com>
2024-02-28 09:23:51 +0100

    test commit

--- gibbs/src/main/java/rules/mining/sampling/GibbsSamplingChoquetTest.java
+++ gibbs/src/main/java/rules/mining/sampling/GibbsSamplingChoquetTest.java
@@ -9,8 +9,8 @@ import rules.mining.requests.SyntheticDataset;
 import rules.mining.rules.BinaryRule;
 import rules.mining.rules.RuleMeasures;
 
-public class GibbsSamplingChoquetTest extends SamplingTest{
-    
+public class GibbsSamplingChoquetTest extends SamplingTest {
+
     public static void main(String[] args) throws Exception {
         SyntheticDataset datasetBuilder = new SyntheticDataset();
 
@@ -18,13 +18,13 @@ public class GibbsSamplingChoquetTest extends SamplingTest{
         // double[][] correlation_matrix = generateSymmetricCorrelationMatrix(size);
         // double[] marginal_probabilities = generateMarginalProbabilities(size);
         // printMatrix(correlation_matrix);
-        
+
         // General parameters
-        int nbItems = 3; 
+        int nbItems = 3;
         int nbSamples = 10;
         int nbIterations = 10;
         int number_rows = 100;
-        String[] measureNames = new String[]{"support", "confidence", "lift", "cosine"};
+        String[] measureNames = new String[] { "support", "confidence", "lift", "cosine" };
 
         List<double[]> allRulesMeasures = new ArrayList<>();
         List<Double> overalls = new ArrayList<>();
@@ -36,34 +36,50 @@ public class GibbsSamplingChoquetTest extends SamplingTest{
         SoftmaxTempChoquet Sampler = new SoftmaxTempChoquet(transactions, nbItems, 3, 2, measureNames);
         LinearOracle Oracle = new LinearOracle(measureNames.length);
 
-        for (int itt=0; itt<nbIterations; itt++) {
+        Sampler.sample(nbSamples * 100);
+        BinaryRule[] allRules = Sampler.getSample();
+
+        for (BinaryRule rule : allRules) {
+            RuleMeasures measurer = new RuleMeasures(rule, number_rows, 0.0);
+            double[] measures = measurer.computeMeasures(measureNames);
+            double score = Oracle.score(measures);
+
+            allRulesMeasures.add(measures);
+            overalls.add(score);
+        }
+        // Convert to arrays
+        double[][] allRulesMeasuresArray = allRulesMeasures.toArray(new double[0][]);
+        double[] overallsArray = overalls.stream().mapToDouble(Double::doubleValue).toArray();
+
+        System.out.println("The oracle's preferences: ");
+        System.out.println(Oracle.topK(allRules, allRulesMeasuresArray, 5));
+
+        for (int itt = 0; itt < nbIterations; itt++) {
             Sampler.sample(nbSamples);
 
             BinaryRule[] sample = Sampler.getSample();
 
-            for(BinaryRule rule : sample) {
+            for (BinaryRule rule : sample) {
                 RuleMeasures measurer = new RuleMeasures(rule, number_rows, 0.0);
                 double[] measures = measurer.computeMeasures(measureNames);
-                double score = Oracle.score(measures); 
+                double score = Oracle.score(measures);
 
                 allRulesMeasures.add(measures);
                 overalls.add(score);
             }
 
             // Convert to arrays
-            double[][] allRulesMeasuresArray = allRulesMeasures.toArray(new double[0][]);
-            double[] overallsArray = overalls.stream().mapToDouble(Double::doubleValue).toArray();
-            
+            // double[][] allRulesMeasuresArray = allRulesMeasures.toArray(new double[0][]);
+            // double[] overallsArray =
+            // overalls.stream().mapToDouble(Double::doubleValue).toArray();
+
             System.out.println("The capacities: " + Arrays.toString(Sampler.getCapacities()));
 
             System.out.println("The sampled rules: ");
             System.out.println(Sampler.toString());
 
-            System.out.println("The oracle's preferences: ");
-            System.out.println(Oracle.topK(sample, allRulesMeasuresArray, 5));
-
             Sampler.train(allRulesMeasuresArray, overallsArray);
         }
-        
+
     }
 }
